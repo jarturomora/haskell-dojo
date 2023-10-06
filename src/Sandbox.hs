8139ba5e9076eefbf61786aaa452a37b4da736c9
Jose Arturo Mora Soto <jarturomora@gmail.com>
2023-10-06 18:59:38 +0000

    Adds note from Class 2

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,3 +1,45 @@
 module Sandbox where
+import Data.Bits (Bits(xor))
+import Data.Char (toUpper)
 
-x = 3 * 4
+-- This is a constant
+_MY_CONSTANT_ :: Char -- Type signature
+_MY_CONSTANT_ = 'B'
+
+-- Unary function - one parameter
+identity :: int -> int -- Function signature
+identity x = x
+
+-- Binary function - two parameters
+add :: Int -> Int -> Int -- Function signature
+add x y = x + y
+
+-- Collection Types --
+-- Pair (different types)
+_PIKACHU_ :: (String, Int)
+_PIKACHU_ = ("Pikachu", 25)
+
+-- List (store single type elements)
+_FIB_ :: [Int]
+_FIB_ = [0, 1, 1, 2, 3, 5, 8, 13]
+
+--Tuples
+_PAIR_ :: (Int, Bool)
+_PAIR_ = (78, True)
+
+_TRIPLE_ :: (Int, Char, Bool)
+_TRIPLE_ = (78, 'J', False)
+
+--Strings (A list of chars)
+_NAME_:: String
+_NAME_ = "Jose Arturo"
+
+-- Type synonyms
+-- We create new types, for example, String is a synonym of a list of Char
+-- They have no signatures
+-- type String = [Char]
+
+-- Custom types
+-- These are new types, no just synonyms
+-- data Type = Contructor
+-- data Bool = False | True
